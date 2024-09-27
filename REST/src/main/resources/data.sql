f621f3d12b2d9315fca6a6d3033f93ea6c3b8d8e
Krzysztof Kucharski <krzysztof85@gmail.com>
2024-09-27 13:21:43 +0200

    Updated data.sql to include entries for the version column in the MeasurementUnit table.

--- REST/src/main/resources/data.sql
+++ REST/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 -- INSERT INTO measure_unit
-INSERT INTO measurement_unit (symbol)
-VALUES ('mm'),
-       ('cm'),
-       ('m'),
-       ('km'),
-       ('mg'),
-       ('g'),
-       ('kg'),
-       ('ml'),
-       ('l'),
-       ('szt');
+INSERT INTO measurement_unit (version, symbol)
+VALUES (1, 'mm'),
+       (1, 'cm'),
+       (1, 'm'),
+       (1, 'km'),
+       (1, 'mg'),
+       (1, 'g'),
+       (1, 'kg'),
+       (1, 'ml'),
+       (1, 'l'),
+       (1, 'szt');
