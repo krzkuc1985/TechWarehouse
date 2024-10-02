9116395f406fb2f2ea3c62e126837bc94f21a922
Krzysztof Kucharski <krzysztof85@gmail.com>
2024-10-02 14:39:52 +0200

    Unnecessary import removed

--- DTO/src/main/java/com/github/krzkuc1985/dto/personaldata/PersonalDataResponse.java
+++ DTO/src/main/java/com/github/krzkuc1985/dto/personaldata/PersonalDataResponse.java
@@ -3,7 +3,6 @@ package com.github.krzkuc1985.dto.personaldata;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
