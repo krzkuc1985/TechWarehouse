116d47b624f536b3258970d3b4fd06208a6e823b
Krzysztof Kucharski <krzysztof85@gmail.com>
2024-10-02 13:40:33 +0200

    Added OpenAPI annotations to AbstractResponse

--- DTO/src/main/java/com/github/krzkuc1985/dto/AbstractResponse.java
+++ DTO/src/main/java/com/github/krzkuc1985/dto/AbstractResponse.java
@@ -1,6 +1,7 @@
 package com.github.krzkuc1985.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ import lombok.Getter;
 public abstract class AbstractResponse {
 
     @JsonProperty("id")
+    @Schema(description = "Unique identifier of the entity", example = "1")
     private Long id;
 
 }
