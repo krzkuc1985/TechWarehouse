21ad198b9dd8ec07f6326c2b0b0f01eceacc3980
Krzysztof Kucharski <krzysztof85@gmail.com>
2025-01-12 11:21:19 +0100

    Added claims to JWT witch authorities

--- REST/src/main/java/com/github/krzkuc1985/rest/authentication/service/AuthenticationServiceImpl.java
+++ REST/src/main/java/com/github/krzkuc1985/rest/authentication/service/AuthenticationServiceImpl.java
@@ -7,9 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,6 +27,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public AuthenticationResponse login(AuthenticationRequest request) throws AuthenticationException {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword()));
         UserDetails userDetails = service.loadUserByUsername(request.getLogin());
+
+        Map<String, Object> claims = Map.of(
+                "Authorities", userDetails.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList())
+        );
+
         String accessToken = jwtService.generateToken(userDetails);
         return AuthenticationResponse.builder().accessToken(accessToken).build();
     }
